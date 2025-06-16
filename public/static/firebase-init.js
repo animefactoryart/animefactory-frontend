b2a19bff851881d5ddcec3f6669aac6406f021f9
Blaz Debeljak <blaz.debeljak77@gmail.com>
2025-06-16 17:48:23 +0200

    Update Firebase config to use getani-ba76e

--- public/static/firebase-init.js
+++ public/static/firebase-init.js
@@ -3,9 +3,12 @@ import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/9.23.
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB0lJ8jUO77xWV_aLtzbENNcoXWnPiBxbg",
-  authDomain: "ai-image-gen-157dc.firebaseapp.com",
-  projectId: "ai-image-gen-157dc"
+  apiKey: "AIzaSyCwV5ALTmOs3bSaHR0nR5nXV5TJ783bj9A",
+  authDomain: "getani-ba76e.firebaseapp.com",
+  projectId: "getani-ba76e",
+  storageBucket: "getani-ba76e.appspot.com",
+  messagingSenderId: "940208374805",
+  appId: "1:940208374805:web:3491bd6a9663f8d2533a1c"
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
